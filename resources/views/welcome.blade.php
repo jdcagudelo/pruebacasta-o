d2d6730bf38f9ae6eb0992927edf8ba83c724103
juan <jdcastano_1@uqvirtual.edu.co>
2020-09-12 22:31:39 -0500

    cambio juan title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>mi primera333 aplicacion 2020</title>
+        <title>prueba repositorio juan</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
