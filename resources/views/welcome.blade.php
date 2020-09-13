8fa2624aa06133b8ce7e9233b8b5429bc579577f
leoHerrera <lfherrerao@uqvirtual.edu.co>
2020-09-12 23:44:15 -0500

    leo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Aplicacion2020 Moreno 24</title>
+        <title>Aplicacion2020 herrera </title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
